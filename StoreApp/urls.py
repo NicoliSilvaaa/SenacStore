bdf349ab6312a435d5ad474ca3b2be5522e29785
Nicoli Silva <nicolialves170@gmail.com>
2024-04-11 22:18:13 -0300

    Adição das paginas de empresa e Cadastro

--- StoreApp/urls.py
+++ StoreApp/urls.py
@@ -5,6 +5,8 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('produtos/', views.produto_lista, name='produto_lista'),
     path('produtos/<int:id>', views.produto_lista_por_departamento, name='produto_lista_por_departamento'),
-    path('produto/<int:id>', views.produto_detalhe, name='produto_detalhe')
+    path('produto/<int:id>', views.produto_detalhe, name='produto_detalhe'),
+    path('sobre_empresa/', views.sobre_empresa, name='sobre_empresa'),
+    path('cadastro/', views.cadastro, name='cadastro')
     
 ]
--- StoreApp/views.py
+++ StoreApp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from StoreApp.models import Departamento, Produto
+from StoreApp.forms import CadastroForm
 
 # Create your views here.
 
@@ -34,8 +35,26 @@ def produto_lista_por_departamento(request, id):
 
 def produto_detalhe(request, id):
     produto = Produto.objects.get(id = id)
+    produtos_relacionados = Produto.objects.filter(departamento_id= produto.departamento).exclude(id = id)[:4]
 
     context = {
-        'produto' : produto
+
+        'produto' : produto,
+        'produtos_relacionados' : produtos_relacionados
+
     }
+
     return render(request, 'produto_detalhes.html', context)
+
+def sobre_empresa(request):
+    return render (request, 'sobre_empresa.html')
+
+
+def cadastro(request):
+
+    formulario = CadastroForm()
+
+    context ={
+        'formulario_cadastro' : formulario
+    }
+    return render(request, 'cadastro.html', context)
